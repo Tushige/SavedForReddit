cd7b427a119f4da9dc96029e563c05a005e213bd
Tushig Ochirkhuyag <tushigochirkhuyag@Tushigs-MacBook-Pro.local>
2019-12-08 23:20:20 -0600

    added auth guard

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import SecureRoute from './components/SecureRoute';
-import Dashboard from './pages/Dashboard';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import WithAuthListener from './decorators/WithAuthListener';
+import SecureRoute from "./components/SecureRoute";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import WithAuthListener from "./decorators/WithAuthListener";
 
 function App(props) {
-    return (
-      <div className="App">
-            <Router>
-                <WithAuthListener>
-                    <Switch>
-                        <Route exact path='/' component={SignIn}/>
-                        <SecureRoute path={'/dashboard'} component={Dashboard}/>
-                        <Route path='*' component={SignUp} />
-                    </Switch>
-                </WithAuthListener>
-            </Router>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Router>
+        <WithAuthListener>
+          <Switch>
+            <Route exact path="/" component={SignIn} />
+            <SecureRoute path={"/dashboard"} component={Dashboard} />
+            <Route path="*" component={SignUp} />
+          </Switch>
+        </WithAuthListener>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
--- src/decorators/WithAuthListener.js
+++ src/decorators/WithAuthListener.js
@@ -1,29 +1,25 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import * as firebase from 'firebase/app';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as firebase from "firebase/app";
 import { useHistory } from "react-router-dom";
 
-function WithAuthListener({children}) {
-    let history = useHistory();
-    useEffect(() => {
-        // listen to auth status change
-        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                history.push('/dashboard');
-            } else {
-                firebase.auth().signOut();
-            }
-        });
-        return function cleanup() {
-            unsubscribe();
-        }
-    }, [])
+function WithAuthListener({ children }) {
+  let history = useHistory();
+  useEffect(() => {
+    // listen to auth status change
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        history.push("/dashboard");
+      } else {
+        firebase.auth().signOut();
+      }
+    });
+    return function cleanup() {
+      unsubscribe();
+    };
+  }, []);
 
-    return (
-        <>
-            {children}
-        </>
-    );
-  }
+  return <>{children}</>;
+}
 
 export default WithAuthListener;
