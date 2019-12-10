ef547c4b92f55c66cb6c297c5c36e935aac4b18e
Tushig <tushige2000@gmail.com>
2019-12-10 10:56:27 -0600

    I finally the reddit token

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 import firebase from '../utils/firebase';
-import rp from 'request-promise'
+// import rp from 'request-promise'
 import {
     REDDIT_CLIENT_ID,
     REDDIT_CLIENT_SECRET,
@@ -18,64 +18,29 @@ function Dashboard(props) {
         }).catch(e => console.error(e))
         props.history.push('/');
     }
-    // function getRedditAccessToken() {
-    //     axios.post(REDDIT_TOKEN_URL, {
-    //         'grant_type': 'authorization_code',
-    //         'code': code,
-    //         'redirect_uri': REDDIT_REDIRECT_URI
-    //     }, {
-    //             'headers': {
-    //                 'Authorization': {
-    //                     'user': REDDIT_CLIENT_ID,
-    //                     'password': REDDIT_CLIENT_SECRET
-    //                 }
-    //             }
-    //         }).then(res => {
-    //             console.log(res);
-    //         }).catch(e => {
-    //             console.error('error retrieving reddit api token');
-    //             console.error(e);
-    //         })
-    // }
-    async function getRedditAccessToken() {
-        const header = new Headers();
-        header.set('Authorization', `Basic ${REDDIT_CLIENT_ID}:${REDDIT_CLIENT_SECRET}`);
-        fetch(REDDIT_TOKEN_URL, {
-            method: 'POST',
-            header,
-            body: JSON.stringify({
-                'grant_type': 'authorization_code',
-                'code': code,
-                'redirect_uri': REDDIT_REDIRECT_URI
-            })
-        }).then(response => {
-            console.log(response)
-            return response.json().then(data => {
-                console.log(data)
-            }).catch(e => {
-                console.error(e)
-            })
-        });
+    function getRedditAccessToken() {
+        const form = new FormData();
+        form.set('grant_type', 'authorization_code');
+        form.set('code', code);
+        form.set('redirect_uri', REDDIT_REDIRECT_URI);
+        const creds = btoa(`${REDDIT_CLIENT_ID}:${REDDIT_CLIENT_SECRET}`);
+        axios.post(REDDIT_TOKEN_URL, form, {
+            'headers': {
+                'Authorization': `Basic ${creds}`
+            },
+            transformRequest: [(data, headers) => {
+                // transform the data
+                delete headers.post['Content-Type'];
+                return data;
+            }]
+        }).then(res => {
+            console.log(res);
+        }).catch(e => {
+            console.error('error retrieving reddit api token');
+            console.error(e);
+        })
     }
-    function getRedditAccessToken2() {
-        // rp({
-        //     json: true,
-        //     baseUrl: 'https://www.reddit.com',
-        //     uri: 'api/v1/access_token',
-        //     method: 'post',
-        //     headers: {
-
-        //     },
-        //     form: {
-        //         'grant_type': 'authorization_code',
-        //         'code': code,
-        //         'redirect_uri': REDDIT_REDIRECT_URI
-        //     }
-        // }).then(token => {
-        //     console.log(token)
-        // })
-    }
-    getRedditAccessToken2();
+    getRedditAccessToken();
     return (
         <>
             <div>Dashboard</div>
